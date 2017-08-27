04b9bd35d713c01b7a39da28d694bcead9c4171c
Daniel Digney <dedigney@gmail.com>
2017-08-27 17:19:13 -0500

    Create comment model & migration, switch new & edit pages to render partials

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-
+  has_many :comments
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { minimum: 3, maximum: 555 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827201617) do
+ActiveRecord::Schema.define(version: 20170827221516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
