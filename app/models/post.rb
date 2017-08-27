2a3634559f96344f153370fdc3a44a908480a6ee
Daniel Digney <dedigney@gmail.com>
2017-08-27 18:09:03 -0500

    Allow comments to be added & deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { minimum: 3, maximum: 555 }
 end
