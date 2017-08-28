012e0fb923cee699a9b87eb8695d3d9d59efae58
Daniel Digney <dedigney@gmail.com>
2017-08-28 08:17:54 -0500

    Add logged_in? helper method, make nav bar dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def logged_in?
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  helper_method :logged_in?
 end
